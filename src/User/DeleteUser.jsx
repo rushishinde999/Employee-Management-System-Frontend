5007898f4bbf730ddc071ff6a2745372a810b972
rushishinde999 <rushikeshrs2002@gmail.com>
2024-08-18 19:33:00 +0530

    add data complete

--- src/User/DeleteUser.jsx
+++ src/User/DeleteUser.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const DeleteUser = () => {
-  return <div>DeleteUser</div>;
+  return (
+    <>
+      <form onSubmit={() => deletedata}>
+        <h2>Delete Employee</h2>
+
+        <div>
+          <input
+            type="text"
+            name=" id"
+            value={id}
+            placeholder=" Enter employee id"
+          />
+
+          <Link>
+            {" "}
+            <button>Delete</button>
+          </Link>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default DeleteUser;
